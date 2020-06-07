be4771c2a17d7f6fb723c361eaa67ccd90b0d90b
fasnimo <fasnimo@gmail.com>
2020-06-06 23:56:38 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,10 @@ class PostsController < ApplicationController
 	  redirect_to post_path(@post)
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params(:title))
@@ -29,7 +33,4 @@ class PostsController < ApplicationController
 		params.require(:post).permit(*args)
 	end
 
-	def edit
-	  @post = Post.find(params[:id])
-	end
 end
